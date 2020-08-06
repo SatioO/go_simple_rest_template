8bdb18fc11834095d82e3a5b0755377d2d69a908
SatioO <vaibhav.satam29991@gmail.com>
2020-08-06 18:14:32 +0530

    added utilities function

--- app/handlers/beers.go
+++ app/handlers/beers.go
@@ -1,27 +1,26 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 
 	"github.com/satioO/go_practices/app/storage"
+	"github.com/satioO/go_practices/app/utilities"
 )
 
 // GetBeers gets all the beers in the inventory
 func GetBeers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	cellar := storage.DB.FindBeers()
-	json.NewEncoder(w).Encode(cellar)
+	utilities.RespondWithJSON(w, http.StatusOK, cellar)
 }
 
 // GetBeer gets all the beer in the inventory
 func GetBeer(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, _ := strconv.ParseInt(vars["id"], 0, 64)
-	w.Header().Set("Content-Type", "application/json")
 	cellar := storage.DB.FindBeer(int(id))
-	json.NewEncoder(w).Encode(cellar)
+	utilities.RespondWithJSON(w, http.StatusOK, cellar)
 }
